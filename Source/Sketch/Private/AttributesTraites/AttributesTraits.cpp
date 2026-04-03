cd23dd66a014b7ec774eedee55dda87dd3641117
danillissimo <danillissimo@mail.ru>
2026-04-03 21:42:54 +0400

    Improved unsupported attribute type UX

--- Source/Sketch/Private/AttributesTraites/AttributesTraits.cpp
+++ Source/Sketch/Private/AttributesTraites/AttributesTraits.cpp
@@ -1,12 +1,21 @@
 #include "AttributesTraites/AttributesTraits.h"
 
 #include "Widgets/Text/STextBlock.h"
+#include "Widgets/Text/STextScroller.h"
 
 TSharedRef<SWidget> sketch::IAttributeImplementation::MakeEditor()
 {
-	return SNew(STextBlock)
-		.Text(INVTEXT("sketch::IAttributeImplementation::MakeEditor has no overload for this type"))
-		.Font(FCoreStyle::GetDefaultFontStyle("Regular", 8));
+	return
+			SNew(SBox)
+			.VAlign(VAlign_Center)
+			[
+				SNew(STextScroller)
+				[
+					SNew(STextBlock)
+					.Text(INVTEXT("sketch::IAttributeImplementation::MakeEditor has no overload for this type"))
+					.Font(FCoreStyle::GetDefaultFontStyle("Regular", 8))
+				]
+			];
 }
 
 FString sketch::IAttributeImplementation::GenerateCode() const
